5f2223e1fded5b42b40dfdfb8a0f37f816d13c63
yalcinsahin55 <yalcin.sahin.55@gmail.com>
2026-08-21 11:58:53 +0300

    Delete lib/seed_data.ts

--- lib/seed_data.ts
+++
@@ -1,7 +0,0 @@
-// 📦 JSON verilerini JS katmanından geçiririz:
-// TypeScript'in büyük JSON dosyalarını tip-check etmesini engeller
-// (build süresi ve bellek kullanımı için).
-import seedData from "./seed_data.json";
-import karterHistory from "./karter_history.json";
-
-export { seedData, karterHistory };
