7ad1ceab6cf3f55a5a75139a800c22bd8f4727c8
yalcinsahin55 <yalcin.sahin.55@gmail.com>
2026-08-21 16:06:36 +0300

    Delete components/Skeleton.jsx

--- components/Skeleton.jsx
+++
@@ -1,8 +0,0 @@
-export default function Skeleton({ className = "", ...props }) {
-  return (
-    <div
-      className={`animate-pulse rounded-lg bg-muted/20 ${className}`}
-      {...props}
-    />
-  );
-}
